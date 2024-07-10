fb06b88da73a70ae4fae566d61c9a2997df8d48e
Kliment Fis <kirilltrun2015@gmail.com>
2024-07-10 21:29:21 +0300

    Данные в JSON

--- api.py
+++ api.py
@@ -3,14 +3,14 @@ from models import Tank
 
 app = FastAPI()
 
-
 @app.get("/")
 async def root():
     return {"message": "Это моя игра, в неё нужно играть вдвоем, это API, для передачи данных между игроками.\nРепо с игрой: https://github.com/KlimentFis/OnlineTanks"}
 
 @app.get("/game")
 async def data():
-    return
+    # return
+    ...
 
 @app.post("/game")
 async def data():
